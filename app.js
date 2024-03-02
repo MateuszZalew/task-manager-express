71bdf82a29610626cf43d647b413bfeb3b2e8b0b
zalesiu <zalewmat31@gmail.com>
2024-03-02 19:03:06 +0100

    feat: add check if there already is a port value in env file in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 const app = express();
-const port = 3000;
+
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
@@ -14,6 +14,7 @@ app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
