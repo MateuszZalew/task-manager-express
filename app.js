b2b9088f39ead6f6e183e885bcddd3fe51d46c73
zalesiu <zalewmat31@gmail.com>
2024-03-02 15:02:36 +0100

    feat: add middlewares and tasks route to app

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require("express");
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const app = express();
+const port = 3000;
+require("dotenv").config();
+
+app.use(express.static("./public"));
+app.use(express.json());
+
+app.use("/api/v1/tasks", tasks);
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, () => {
+      console.log(`Server is listening at http://localhost:${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
